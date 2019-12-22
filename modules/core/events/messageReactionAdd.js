07a18c5fc33964d63eb0a51aeab9255f6be763be
Caltrop <caltropuwu@gmail.com>
2019-12-22 14:13:40 +0100

    anti raid stuff

--- modules/core/events/messageReactionAdd.js
+++ modules/core/events/messageReactionAdd.js
@@ -9,14 +9,22 @@ user                   User                   The user that applied the emoji or
 module.exports = {
     async execute(client, reaction, user) {
         let message_id = client.cfg.ruleAccept;
-        if(reaction.message.id !== message_id)return;
+        if(reaction.message.id !== message_id) return;
         reaction.remove();
         var member = reaction.message.guild.member(user);
-        let role = (member.guild.roles.find(role => role.name === "Welcome")) 
-        member.removeRole(role)
-        .then(() => 
-        {
-            console.log(`Removed role ${role} from ${member.displayName}`);
-        });
+
+        if(client.scrips.isSuspect(user) && member.joinedTimestamp > Date.now() - 600000) {
+            let embed = client.scripts.getEmbed()
+                .setAuthor(`Please wait ${client.time(member.joinedTimestamp - (Date.now() - 600000))}`)
+                .setColor(client.constants.red.hex)
+                setTimestamp();
+            return user.send({embed});
+        } else {
+            let role = (member.guild.roles.find(role => role.name === "Welcome")) 
+            member.removeRole(role)
+            .then(() => {
+                console.log(`Removed role ${role} from ${member.displayName}`);
+            });
+        }
     }
 }
