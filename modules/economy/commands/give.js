430e5d1e6b6d78ec110f2c8badcafe350ec5f868
CheeseBoye <jeanpaul_farrugia@hotmail.com>
2020-03-08 14:12:38 +0100

    A single update
    added a confirmation thing for giving money
    
    inspired by people giving money to another person with cheese in their name instead of me
    
    coming up next: No updates, i just decided to push an empty commit

--- modules/economy/commands/give.js
+++ modules/economy/commands/give.js
@@ -27,25 +27,51 @@ module.exports = {
             let coins = money.coins
             if (coins - giveNumber < 0) return message.reply(`You don't have enough coins (${client.scripts.numComma(coins)}/${client.scripts.numComma(parseInt(giveNumber))})`);
 
-            client.updateMoney(message.author.id, -Math.abs(giveNumber));
-            client.updateMoney(receivingUser.id, giveNumber);
+            message.channel.send(client.scripts.getEmbed().setAuthor('Are you sure?', message.author.displayAvatarURL).setDescription(`You are about to give ${client.scripts.numComma(parseInt(giveNumber))}${client.cfg.curName} to ${receivingUser.user.tag}!`).setColor("#00FF00").setTimestamp())
+                .then(async msg => {
+                    var emojis = ['✅', '❎']
+                    await msg.react(emojis[0]);
+                    await msg.react(emojis[1]);
+                    const filter = (reaction, user) => {
+                        return user.id != msg.author.id;
+                    };
+                    var collector = msg.createReactionCollector(filter, { time: 60000 });
+                    collector.on('collect', reaction => {
+                        if (!emojis.includes(reaction.emoji.name)) return msg.reactions.last().remove(reaction.users.first().id);
+                        for (var u of reaction.users) {
+                            if ((u[0] != message.author.id) && (u[0] != msg.author.id)) {
+                                return reaction.remove(u[0]);
+                            };
+                        };
+                        collector.stop();
+                        if (reaction.emoji.name == emojis[0]) {
 
-            console.log(console.color.green(`[Economy]`), `${message.author.username} gave ${giveNumber}${client.cfg.curName} to ${receivingUser.user.username}`);
+                            client.updateMoney(message.author.id, -Math.abs(giveNumber));
+                            client.updateMoney(receivingUser.id, giveNumber);
 
-            var giveUnicode = '💸';
-            if (giveNumber === 413)
-                giveUnicode = '<:howHigh:663360611782230016>';
-            else if (giveNumber === 420)
-                giveUnicode = '<:ralGasp:654014647460036619>';
-            else if (giveNumber === 69)
-                giveUnicode = '😏';
-            var GiveEmbed = client.scripts.getEmbed()
-                .setAuthor(message.member.displayName, message.author.avatarURL, message.author.avatarURL)
-                .addField(giveUnicode, `Gave \`${client.scripts.numComma(parseInt(giveNumber))}${giveNumber !== 69 ? ' ' + client.cfg.curName : ''}\` to **${receivingUser.displayName}**`)
-                .setColor("#00FF00")
+                            console.log(console.color.green(`[Economy]`), `${message.author.tag} gave ${giveNumber}${client.cfg.curName} to ${receivingUser.user.tag}`);
 
-            message.channel.send({ embed: GiveEmbed })
+                            var giveUnicode = '💸';
+                            if (giveNumber === 413)
+                                giveUnicode = '<:howHigh:663360611782230016>';
+                            else if (giveNumber === 420)
+                                giveUnicode = '<:ralGasp:654014647460036619>';
+                            else if (giveNumber === 69)
+                                giveUnicode = '😏';
+                            var GiveEmbed = client.scripts.getEmbed()
+                                .setAuthor(message.member.displayName, message.author.avatarURL, message.author.avatarURL)
+                                .addField(giveUnicode, `Gave \`${client.scripts.numComma(parseInt(giveNumber))}${giveNumber !== 69 ? ' ' + client.cfg.curName : ''}\` to **${receivingUser.displayName}**`)
+                                .setColor("#00FF00")
 
+                            message.channel.send({ embed: GiveEmbed })
+                        } else {
+                            return
+                        }
+                    });
+                    collector.on('end', () => {
+                        msg.delete();
+                    });
+                });
         });
     }
 }
