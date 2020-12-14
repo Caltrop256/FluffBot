d7e266e6643d7daa5fb0033a8644adbc8e7c48e4
CheeseBoye <jeanpaul_farrugia@hotmail.com>
2020-12-14 13:05:02 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# v4
+Work in progress rewrite of [FluffBot](https://github.com/Caltrop256/FluffBot/tree/v3-Latest)
--- library/cvUnits/percentError.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function (expected, actual) {
-  return Math.abs((expected - actual) / actual);
-}
